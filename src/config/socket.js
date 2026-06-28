d819aeb3d9bd301fdb2993c3d734ba71a4a2bd83
Asser-wael <aserweal@gmail.com>
2026-06-28 19:05:50 +0300

    Add React project

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 
 let io;
+let onlineUsers = 0;
 
 export const initSocket = (server) => {
   io = new Server(server, {
@@ -12,15 +13,26 @@ export const initSocket = (server) => {
 
   io.on("connection", (socket) => {
     console.log("User Connected:", socket.id);
+
+    onlineUsers++;
+
+    // إرسال العدد لكل العملاء
+    io.emit("onlineUsers", onlineUsers);
+
     socket.on("join-table", (tableNumber) => {
       socket.join(`table-${tableNumber}`);
     });
+
     socket.on("joinAdminRoom", () => {
       socket.join("admin");
     });
 
     socket.on("disconnect", () => {
       console.log("User Disconnected");
+
+      onlineUsers--;
+
+      io.emit("onlineUsers", onlineUsers);
     });
   });
 
